a7185894052bc8d5f1b94cc008228648e3e0a32e
belliedmonkey <belliedmonkey@gmail.com>
2024-02-17 11:58:54 +0800

    lux

--- api/index.go
+++ api/index.go
@@ -3,24 +3,17 @@ package handler
 import (
 	"fmt"
 	"net/http"
-	"os"
 
-	"github.com/fatih/color"
-
-	"github.com/iawia002/lux/app"
+	"github.com/iawia002/lux/extractors"
 )
 
 func Handler(w http.ResponseWriter, r *http.Request) {
-	var args = []string{"-i", "https://www.youtube.com/watch?v=6g4dkBF5anU"}
-	if err := app.New().Run(args); err != nil {
-		fmt.Fprintf(
-			w,
-			"Run %s failed: %s\n",
-			color.CyanString("%s", app.Name), color.RedString("%v", err),
-		)
-		os.Exit(1)
-	}
+	var (
+		data []*extractors.Data
+		err  error
+	)
 
-	fmt.Fprintf(w, "<h1>Hello from Go!</h1>")
+	data, err = extractors.Extract("https://www.youtube.com/watch?v=Gnbch2osEeo", extractors.Options{})
+	fmt.Fprintf(w, "<h1>Hello from Go!</h1>"+data[0].Title+err.Error())
 
 }
